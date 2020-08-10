6c9f91c99866928cc3243ad7c4daeb28a60cc9b5
Yash Goswami <yashgoswami148@gmail.com>
2020-08-10 18:30:08 +0530

    Authentication and restriction

--- projfrontend/src/Routes.js
+++ projfrontend/src/Routes.js
@@ -1,19 +1,25 @@
-import React from 'react';
-import {BrowserRouter,Switch,Route} from 'react-router-dom';
-import Home from './core/Home';
-import Signin from './user/Signin';
-import Signup from './user/Signup';
+import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import AdminRoute from "./auth/helper/AdminRoutes";
+import PrivateRoute from "./auth/helper/PrivateRoutes";
+import Home from "./core/Home";
+import AdminDashBoard from "./user/AdminDashBoard";
+import Signin from "./user/Signin";
+import Signup from "./user/Signup";
+import UserDashBoard from "./user/UserDashBoard";
 
 const Routes = () => {
-    return (
-        <BrowserRouter>
-            <Switch>
-                <Route path="/" exact component={Home}/>
-                <Route path="/signup" exact component={Signup}/>
-                <Route path="/signin" exact component={Signin}/>
-            </Switch>
-        </BrowserRouter>
-    );
-}
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/signup" exact component={Signup} />
+        <Route path="/signin" exact component={Signin} />
+        <PrivateRoute path="/user/dashboard" exact component={UserDashBoard} />
+        <AdminRoute path="/admin/dashboard" exact component={AdminDashBoard} />
+      </Switch>
+    </BrowserRouter>
+  );
+};
 
 export default Routes;
